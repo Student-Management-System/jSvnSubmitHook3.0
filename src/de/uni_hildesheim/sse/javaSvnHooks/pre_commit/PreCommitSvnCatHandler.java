b1094f64803202da0bc093e4643fdd40be950564
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-08-06 07:09:14 +0200

    Bug fix by Adam to avoid IllegalArgumentExceptions at pre commit hook
    Uses a process to checkout the data during pre-commit check instead of
    SvnKit.
    SvnKit produces in some situations a IllegalArgumentException
      at java.base/sun.nio.ch.FileChannelImpl.position(FileChannelImpl.java:351)
    	at org.tmatesoft.svn.core.internal.io.fs.FSFile.fill(FSFile.java:461)
    	at org.tmatesoft.svn.core.internal.io.fs.FSFile.read(FSFile.java:366)
    	at org.tmatesoft.svn.core.internal.io.fs.FSFile.readLine(FSFile.java:146)
    	at org.tmatesoft.svn.core.internal.io.fs.FSInputStream.readRepresentationLine(FSInputStream.java:249)
    	at org.tmatesoft.svn.core.internal.io.fs.FSInputStream.buildRepresentationList(FSInputStream.java:202)
    	at org.tmatesoft.svn.core.internal.io.fs.FSInputStream.<init>(FSInputStream.java:64)
    	at org.tmatesoft.svn.core.internal.io.fs.FSInputStream.createDeltaStream(FSInputStream.java:86)
    	at org.tmatesoft.svn.core.internal.io.fs.FSRoot.getFileStreamForPath(FSRoot.java:327)
    	at org.tmatesoft.svn.core.wc.admin.SVNLookClient.catFile(SVNLookClient.java:1276)
    	at org.tmatesoft.svn.core.wc.admin.SVNLookClient.doCat(SVNLookClient.java:388)
    	at de.uni_hildesheim.sse.javaSvnHooks.pre_commit.PreCommitSvnCatHandler.handleEntry(PreCommitSvnCatHandler.java:62)

--- src/de/uni_hildesheim/sse/javaSvnHooks/pre_commit/PreCommitSvnCatHandler.java
+++ src/de/uni_hildesheim/sse/javaSvnHooks/pre_commit/PreCommitSvnCatHandler.java
@@ -1,9 +1,11 @@
 package de.uni_hildesheim.sse.javaSvnHooks.pre_commit;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,6 +19,7 @@ import org.tmatesoft.svn.core.wc.admin.SVNAdminPath;
 import org.tmatesoft.svn.core.wc.admin.SVNChangeEntry;
 import org.tmatesoft.svn.core.wc.admin.SVNLookClient;
 
+import de.uni_hildesheim.sse.javaSvnHooks.Configuration;
 import de.uni_hildesheim.sse.javaSvnHooks.logging.Logger;
 
 /**
@@ -45,59 +48,85 @@ class PreCommitSvnCatHandler implements ISVNChangeEntryHandler, ISVNTreeHandler 
         this.configuration = configuration;
         changedFiles = new ArrayList<String>();
     }
-
-    @Override
-    public void handleEntry(SVNChangeEntry entry) throws SVNException {
-        if (entry.getKind() == SVNNodeKind.FILE && !isFileIgnored(entry.getPath())) {
+    
+    private void checkoutWithSvnlookProcess(String path, File target) {
+        
+        ProcessBuilder pb = new ProcessBuilder(
+            "/usr/bin/svnlook",
+            "cat",
+            "--transaction",
+            configuration.getTransaction(),
+            configuration.getRepository().getAbsolutePath(),
+            path
+        );
+        
+        pb.redirectOutput(target);
+        pb.redirectError(Redirect.PIPE);
+        
+        try {
+            Process proc = pb.start();
             
-            changedFiles.add(entry.getPath());
-            
-            File file = new File(configuration.getCheckoutDir(), entry.getPath());
-            file.getParentFile().mkdirs();
-            
-            if (entry.getType() != SVNChangeEntry.TYPE_DELETED) {
-                try {
-                    FileOutputStream fos = new FileOutputStream(file);
-                    
-                    lookClient.doCat(configuration.getRepository(), entry.getPath(),
-                            configuration.getTransaction(), fos);
-                    
-                    fos.close();
-                    
-                } catch (FileNotFoundException e) {
-                    Logger.INSTANCE.logException(e, false);
-                } catch (IOException e) {
-                    Logger.INSTANCE.logException(e, false);
-                }
+            BufferedReader stderrReader = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+            StringBuilder stderr = new StringBuilder();
+            String line;
+            while ((line = stderrReader.readLine()) != null) {
+                stderr.append(line);
             }
             
+            int result = proc.waitFor();
+            
+            if (result != 0) {
+                Logger.INSTANCE.log("WARNING: svnlook exited with " + result);
+                Logger.INSTANCE.log("svnlook stderr:\n" + stderr.toString());
+            }
+            
+        } catch (IOException | InterruptedException e) {
+            Logger.INSTANCE.logException(e, false);
+        }
+    }
+    
+    @SuppressWarnings("unused")
+    private void checkoutWithSvnkit(String path, File target) throws SVNException {
+        try (FileOutputStream fos = new FileOutputStream(target)) {
+            
+            lookClient.doCat(configuration.getRepository(), path, configuration.getTransaction(), fos);
+            
+        } catch (IOException e) {
+            Logger.INSTANCE.logException(e, false);
+        }
+    }
+
+    private void writeFileToCheckoutDir(String path) throws SVNException {
+        changedFiles.add(path);
+        
+        File target = new File(configuration.getCheckoutDir(), path);
+        File dir = target.getParentFile();
+        if (!dir.isDirectory() && !dir.mkdirs()) {
+            Logger.INSTANCE.log("WARNING: Could not create directory " + dir);
+        }
+        
+//        checkoutWithSvnkit(path, target);
+        
+        checkoutWithSvnlookProcess(path, target);
+    }
+    
+    @Override
+    public void handleEntry(SVNChangeEntry entry) throws SVNException {
+//        Logger.INSTANCE.log("handleEntry(path=" + entry.getPath() + ", type=" + entry.getType() + ", kind=" + entry.getKind() + ")");
+        
+        if (entry.getKind() == SVNNodeKind.FILE && !isFileIgnored(entry.getPath())
+                && entry.getType() != SVNChangeEntry.TYPE_DELETED) {
+            
+            writeFileToCheckoutDir(entry.getPath());
         }
     }
 
     @Override
     public void handlePath(SVNAdminPath path) throws SVNException {
+//        Logger.INSTANCE.log("handlePath(path=" + path.getPath() + ", isDir=" + path.isDir() + ")");
+        
         if (!path.isDir() && !isFileIgnored(path.getPath())) {
-            
-            File file = new File(configuration.getCheckoutDir(), path.getPath());
-            
-            changedFiles.add(path.getPath());
-            
-            file.getParentFile().mkdirs();
-            
-            try {
-                FileOutputStream fos = new FileOutputStream(file);
-                
-                lookClient.doCat(configuration.getRepository(), path.getPath(),
-                        configuration.getTransaction(), fos);
-                
-                fos.close();
-                
-            } catch (FileNotFoundException e) {
-                Logger.INSTANCE.logException(e, false);
-            } catch (IOException e) {
-                Logger.INSTANCE.logException(e, false);
-            }
-            
+            writeFileToCheckoutDir(path.getPath());
         }
     }
     
